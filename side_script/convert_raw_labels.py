a5cfa4727ed2f9dc09a8d193cfa05551604d1a65
emilien.briant@gmail.com <typomur3>
2020-01-28 09:31:53 +0900

    Updates: - dataset processing methods

--- side_script/convert_raw_labels.py
+++ side_script/convert_raw_labels.py
@@ -2,22 +2,24 @@ import pandas as pd
 import os
 
 
-if __name__ == "__main__":
-    data_folder = "data/raw_labels"
+def convert_raw_labels(src_folder, dst_file):
     df = pd.DataFrame(columns=["video", "frame", "name", "target"])
     max_frames = [9900, 8000, 10000, 12000]
-    for file in os.listdir(data_folder):
+    for file in os.listdir(src_folder):
         tmp_df = pd.DataFrame(columns=["video", "frame", "name", "target"])
         video = file[:-4]
-        data = pd.read_csv(os.path.join(data_folder, file))
+        data = pd.read_csv(os.path.join(src_folder, file))
         if len(max_frames) > 0:
             data = data.loc[data["frame"] < max_frames.pop(0)]
         for name in data.columns[1:]:
             name_df = data[["frame", name]]
-            name_df.columns= ["frame", "target"]
+            name_df.columns = ["frame", "target"]
             name_df.insert(0, "video", video)
             name_df.insert(2, "name", name)
 
             df = df.append(name_df)
+    df.to_csv(dst_file, index=False)
 
-    df.to_csv("data/labels/labels.csv", index=False)
+
+if __name__ == "__main__":
+    convert_raw_labels("data/raw_labels", "data/labels/labels_originals.csv")
--- side_script/separate_dataset.py
+++ side_script/separate_dataset.py
@@ -1,22 +1,22 @@
 import pandas as pd
-import numpy.random as rdm
 
-df = pd.read_csv("data/labels/labels.csv")
 
-patch_size = 40
-sep_factor = 0.8
-df_copy = df.copy()
+def separate_dataset(src_file, train_file, test_file, sep_factor=0.8, patch_size=1):
+    df = pd.read_csv(src_file)
+    df_copy = df.copy()
+    if patch_size <= 1:
+        t2 = df_copy.sample(frac=(1 - sep_factor), random_state=0)
+    else:
+        t1 = df_copy.sample(frac=(1 - sep_factor) / (patch_size * 5), random_state=0)
+        t2 = pd.DataFrame(columns=t1.columns)
+        for index, row in t1.iterrows():
+            for k in range(0, patch_size):
+                tmp_row = df.iloc[int(row.name) + k]
+                rows_list = df.loc[(df['video'] == tmp_row["video"]) & (df['frame'] == tmp_row['frame'])]
+                t2 = t2.append(rows_list)
+        t2.drop_duplicates()
 
-t1 = df_copy.sample(frac=(1-sep_factor)/(patch_size*5.2), random_state=0)
-t2 = pd.DataFrame(columns=t1.columns)
-for index, row in t1.iterrows():
-    for k in range(0, patch_size):
-        tmp_row = df.iloc[int(row.name) + k]
-        rows_list = df.loc[(df['video'] == tmp_row["video"]) & (df['frame'] == tmp_row['frame'])]
-        t2 = t2.append(rows_list)
-t2.drop_duplicates()
-
-train = df_copy.drop(t2.index)
-print(len(t2), len(train), len(df))
-train.to_csv(r'data/labels/train_labels_frame_patches.csv', index=False)
-t2.to_csv(r'data/labels/test_labels_frame_patches.csv', index=False)
+    train = df_copy.drop(t2.index)
+    print(len(t2), len(train), len(df))
+    train.to_csv(train_file, index=False)
+    t2.to_csv(test_file, index=False)
